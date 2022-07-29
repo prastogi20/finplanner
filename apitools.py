fe0dfc52bc21edda849775a2539622c6d134c094
Pragya <pragya.fintech@gmail.com>
2022-07-29 12:21:23 +1000

    updated apitools for comments

--- apitools.py
+++ apitools.py
@@ -1,5 +1,4 @@
 # Initial imports
-# Initial imports
 import os
 import requests
 from pandas import DataFrame
@@ -10,7 +9,8 @@ import alpaca_trade_api as tradeapi
 class APIcalls:
     def __init__(self, envpath: Path = None):
         """
-        
+        APIcalls constructor, load's environment and initializes key variables
+        envpath : specifies path of environment file 
         """
         if(envpath == None):
             load_dotenv()
@@ -22,6 +22,11 @@ class APIcalls:
         
     
     def get_cryptos_price(self, urls:{}) -> {}:
+        """
+        get_cryptos_price fetches Crypto data e.g. prices, volume using request library
+        urls : dictionary of {<Crypto symbol> : <url to make request call>}
+        returns: dictionary of {<Crypto symbol> : <response json object}
+        """
         responsedic = {}
         for url in urls:            
             response = requests.get(urls[url])
@@ -31,6 +36,15 @@ class APIcalls:
     
     
     def get_alpca_bars(self,tickers: [],timeframe: str,startdate:str = None,enddate: str = None) -> DataFrame:
+        """
+        get_alpca_bars uses alpca sdk to get tickers bars.
+        
+        tickers : list of string that represents tickers symbols
+        timeframe : represents duration for which data is required e.g. "1Day"
+        startdate :  start date
+        enddate : end date        
+        return : DataFrame containing ticker historical data fetched from alpaca api
+        """
         alpaca = tradeapi.REST(
             self._alpaca_api_key,
             self._alpaca_secret_key,
